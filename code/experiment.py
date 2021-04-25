e3a58f804b70616cb034615016d852c78dc9371d
alexhad6 <alexhad6@gmail.com>
2021-04-24 18:12:28 -0700

    Added plot and correlation coefficients

--- code/experiment.py
+++ code/experiment.py
@@ -3,6 +3,12 @@
 import argparse
 import pandas as pd
 import numpy as np
+from scipy.stats import pearsonr, spearmanr
+
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
+
 import incongruity
 import glove
 
@@ -11,42 +17,57 @@ def extract_original_phrase(headline):
     end = headline.index('>')
     return headline[start+1:end-1]
 
+def combined_data(dataframes, metric):
+    metric_values = []
+    meanGrades = []
+    for dataframe in dataframes:
+        for i, row in dataframe.iterrows():
+            if not pd.isna(row[metric]):
+                metric_values.append(row[metric])
+                meanGrades.append(row.meanGrade)
+    return metric_values, meanGrades
+
+def plot_metric(metric_values, meanGrades, ylabel, plotname):
+    fig = plt.figure(figsize=(8, 8))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.scatter(meanGrades, metric_values, c='b', s=12, alpha=0.1)
+    plt.xlabel('Mean Humor Grade')
+    plt.ylabel(ylabel)
+    plt.savefig(plotname)
+
 def main(args):
     # Load train and test data
-    train = pd.read_csv(args.train)    
-    test = pd.read_csv(args.test)
+    train = pd.read_csv('../data/task-1/train.csv')    
+    test = pd.read_csv('../data/task-1/test.csv')  
+    funlines = pd.read_csv('../data/task-1/train_funlines.csv')
     train['original_phrase'] = train.original.apply(extract_original_phrase)
     test['original_phrase'] = test.original.apply(extract_original_phrase)
+    funlines['original_phrase'] = funlines.original.apply(extract_original_phrase)
 
     # Load GloVe vectors
     glove_vectors, glove_words = glove.load_glove_vectors(args.npyFILE)
 
     # Calculate cosine similarity metrics
     train['original_vs_edit'] = train.apply(lambda row: incongruity.original_vs_edit(row.original_phrase, row.edit, glove_words, glove_vectors), axis=1)
-    
-    # Some GloVe vector tests
-    # print(f'original: {train.original_phrase[25]}, edit: {train.edit[25]}, {train.metric1[25]}')
-    # print(f'original: {train.original_phrase[22]}, edit: {train.edit[22]}, {train.metric1[22]}')
-    # print(f'original: {train.original_phrase[9647]}, edit: {train.edit[9647]}, {train.metric1[9647]}')
-    # print(train[train.metric1 == train.metric1.min()])
-    # print(train[train.metric1 == train.metric1.max()])
-    # print(incongruity.metric1(train.original_phrase[1], train.edit[1], glove_words, glove_vectors))
-    
+    test['original_vs_edit'] = test.apply(lambda row: incongruity.original_vs_edit(row.original_phrase, row.edit, glove_words, glove_vectors), axis=1)
+    funlines['original_vs_edit'] = funlines.apply(lambda row: incongruity.original_vs_edit(row.original_phrase, row.edit, glove_words, glove_vectors), axis=1)
+
+    # Plot original_vs_edit vs meanGrade
+    original_vs_edit, meanGrades = combined_data([train, test, funlines], 'original_vs_edit')
+    plot_metric(original_vs_edit, meanGrades, 'Original vs. Edit Cosine Similarity', args.plot)
+    print(f'Pearson correlation: {pearsonr(original_vs_edit, meanGrades)}')
+    print(f'Spearman correlation: {spearmanr(original_vs_edit, meanGrades)}')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('npyFILE',
-        type=argparse.FileType('rb'),
+    parser.add_argument('npyFILE', type=argparse.FileType('rb'),
         help='an .npy file to read the saved numpy data from')
-    parser.add_argument('train',
-        type=argparse.FileType('r'),
-        help='a CSV file of the train data')
-    parser.add_argument('test',
-        type=argparse.FileType('r'),
-        help='a CSV file of the test data')
+    # parser.add_argument('train', type=str,
+    #     help='a CSV file of the train data')
+    # parser.add_argument('test', type=str,
+    #     help='a CSV file of the test data')
+    parser.add_argument('plot', type=str,
+        help='name of file to write plot to')
     main(parser.parse_args())
 
-
-# python experiment.py ../glove/glove.6B.50d.npy ../data/humicroedit/task-1/train.csv ../data/humicroedit/task-1/dev.csv
-
-# python experiment.py ../data/funlines/task-1/train_funlines.csv ../data/humicroedit/task-1/dev.csv
+# python experiment.py ../glove/glove.6B.50d.npy plot-all.png
