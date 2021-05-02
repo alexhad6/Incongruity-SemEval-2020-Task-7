ef6188eabe40afbf97494d7e6e7420de2b509365
alexhad6 <alexhad6@gmail.com>
2021-05-01 17:07:03 -0700

    Created metric 2, added RMSE

--- code/experiment.py
+++ code/experiment.py
@@ -1,9 +1,10 @@
 # Run the experiment
 
+import math
 import argparse
 import pandas as pd
 import numpy as np
-from scipy.stats import pearsonr, spearmanr
+from scipy.stats import pearsonr, spearmanr, linregress
 
 import matplotlib
 matplotlib.use('Agg')
@@ -17,46 +18,91 @@ def extract_original_phrase(headline):
     end = headline.index('>')
     return headline[start+1:end-1]
 
-def combined_data(dataframes, metric):
-    metric_values = []
-    meanGrades = []
-    for dataframe in dataframes:
-        for _, row in dataframe.iterrows():
-            if not pd.isna(row[metric]):
-                metric_values.append(row[metric])
-                meanGrades.append(row.meanGrade)
-    return metric_values, meanGrades
-
-def plot_metric(metric_values, meanGrades, ylabel, plotname):
-    fig = plt.figure(figsize=(8, 8))
+def plot_metric(metric_values, meanGrades, xlabel, plotname):
+    fig = plt.figure(figsize=(8, 4.5))
     ax = fig.add_subplot(1, 1, 1)
-    ax.scatter(meanGrades, metric_values, c='b', s=12, alpha=0.1)
-    plt.xlabel('Mean Humor Grade')
-    plt.ylabel(ylabel)
+    ax.scatter(metric_values, meanGrades, c='b', s=12, alpha=0.1)
+    p = np.poly1d(np.polyfit(metric_values, meanGrades, 1))
+    plt.plot(metric_values, p(metric_values), 'r', linewidth=2)
+    plt.xlabel(xlabel)
+    plt.ylabel('Mean Humor Grade')
     plt.savefig(plotname)
+
+def statistics(metric_values, mean_grades):
+    p = np.poly1d(np.polyfit(metric_values, mean_grades, 1))
+    n = len(mean_grades)
+    slope, intercept, r_value, p_value, std_err = linregress(metric_values, mean_grades)
+    avg_grade = sum(mean_grades)/n
+    RMSE = math.sqrt(sum((p(metric_values[i]) - mean_grades[i])**2 for i in range(n))/n)
+    RMSE_avg = math.sqrt(sum((avg_grade - mean_grades[i])**2 for i in range(n))/n)
+    print(f'Linear regression equation: {p}')
+    print(f'Pearson correlation: {pearsonr(metric_values, mean_grades)}')
+    print(f'Spearman correlation: {spearmanr(metric_values, mean_grades)}')
+    print(f'n: {n}')
+    print(f'slope: {slope}, intercept: {intercept}, r_value: {r_value}, p_value: {p_value}, std_err: {std_err}')
+    # print(f'RMSE: {RMSE}')
+    # print(f'RMSE Average: {RMSE_avg}')
+    print()
+
+def export_to_csv(metric_values, mean_grades, metric_name):
+    export = pd.DataFrame()
+    export['mean_grades'] = mean_grades
+    export[metric_name] = metric_values
+    export.to_csv(metric_name+'.csv')
+
+def run_metric(train, test, metric, metric_name, axis_name):
+    train[metric_name] = train.apply(metric, axis=1)
+    train_notnull = train[train[metric_name].notnull()]
+    metric_values = train_notnull[metric_name].values.tolist()
+    mean_grades = train_notnull['meanGrade'].values.tolist()
+    plot_metric(metric_values, mean_grades, axis_name, f'{metric_name}.png')
+    print(metric_name)
+    statistics(metric_values, mean_grades)
+
+    model = np.poly1d(np.polyfit(metric_values, mean_grades, 1))
+    average_grade = sum(mean_grades)/len(mean_grades)
+
+    test[metric_name] = test.apply(metric, axis=1)
+    test_notnull = test[test[metric_name].notnull()]
+    test_metric_values = test_notnull[metric_name].values.tolist()
+    test_mean_grades = test_notnull['meanGrade'].values.tolist()
+    n = len(test_mean_grades)
+    RMSE = math.sqrt(sum((model(test_metric_values[i]) - test_mean_grades[i])**2 for i in range(n))/n)
+    RMSE_avg = math.sqrt(sum((average_grade - test_mean_grades[i])**2 for i in range(n))/n)
+    print(f'RMSE: {RMSE}')
+    print(f'RMSE average: {RMSE_avg}')
 
 def main(args):
     # Load train and test data
-    train = pd.read_csv('../data/task-1/train.csv')    
-    test = pd.read_csv('../data/task-1/test.csv')  
-    funlines = pd.read_csv('../data/task-1/train_funlines.csv')
+    humicroedit_train = pd.read_csv('../data/task-1/train.csv')
+    funlines_train = pd.read_csv('../data/task-1/train_funlines.csv')
+    train = pd.concat([humicroedit_train, funlines_train])
+    test = pd.read_csv('../data/task-1/test.csv')
     train['original_phrase'] = train.original.apply(extract_original_phrase)
     test['original_phrase'] = test.original.apply(extract_original_phrase)
-    funlines['original_phrase'] = funlines.original.apply(extract_original_phrase)
 
     # Load GloVe vectors
     glove_vectors, glove_words = glove.load_glove_vectors(args.npyFILE)
 
-    # Calculate cosine similarity metrics
-    train['original_vs_edit'] = train.apply(lambda row: incongruity.original_vs_edit(row.original_phrase, row.edit, glove_words, glove_vectors), axis=1)
-    test['original_vs_edit'] = test.apply(lambda row: incongruity.original_vs_edit(row.original_phrase, row.edit, glove_words, glove_vectors), axis=1)
-    funlines['original_vs_edit'] = funlines.apply(lambda row: incongruity.original_vs_edit(row.original_phrase, row.edit, glove_words, glove_vectors), axis=1)
+    # Run original vs. edit (metric 1)
+    metric1 = lambda row: incongruity.original_vs_edit(row.original_phrase, row.edit, glove_words, glove_vectors)
+    run_metric(train, test, metric1, 'original_vs_edit', 'Original vs. Edit Cosine Similarity')
+    print()
 
-    # Plot original_vs_edit vs meanGrade
-    original_vs_edit, meanGrades = combined_data([train, test, funlines], 'original_vs_edit')
-    plot_metric(original_vs_edit, meanGrades, 'Original vs. Edit Cosine Similarity', args.plot)
-    print(f'Pearson correlation: {pearsonr(original_vs_edit, meanGrades)}')
-    print(f'Spearman correlation: {spearmanr(original_vs_edit, meanGrades)}')
+    # # Run edit vs. neighbors (metric 2) with window 1
+    # metric2_window1 = lambda row: incongruity.edit_vs_neighbors(row.original, row.edit, glove_words, glove_vectors, window_size=1)
+    # run_metric(train, test, metric2_window1, 'edit_vs_neighbors_window1', 'Edit vs. Neighbors Cosine Similarity (window=1)')
+    # print()
+
+    # # Run edit vs. neighbors (metric 2) with window 3
+    # metric2_window3 = lambda row: incongruity.edit_vs_neighbors(row.original, row.edit, glove_words, glove_vectors, window_size=3)
+    # run_metric(train, test, metric2_window3, 'edit_vs_neighbors_window3', 'Edit vs. Neighbors Cosine Similarity (window=3)')
+    # print()
+
+    # # Calculate edit vs. neighbors (metric 2) with no window
+    # metric2 = lambda row: incongruity.edit_vs_neighbors(row.original, row.edit, glove_words, glove_vectors)
+    # run_metric(train, test, metric2, 'edit_vs_neighbors', 'Edit vs. Neighbors Cosine Similarity (no window)')
+    # print()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -66,13 +112,67 @@ if __name__ == '__main__':
     #     help='a CSV file of the train data')
     # parser.add_argument('test', type=str,
     #     help='a CSV file of the test data')
-    parser.add_argument('plot', type=str,
-        help='name of file to write plot to')
+    # parser.add_argument('plot', type=str,
+    #     help='name of file to write plot to')
     main(parser.parse_args())
 
-# python experiment.py ../glove/glove.6B.300d.npy plot-all.png
-
+# python experiment.py ../glove/glove.6B.50d.npy plot-all.png
 
 # original_vs_edit w/ 300D GloVe
 # Pearson correlation: (-0.17739550138691068, 1.0680352459856034e-143)
 # Spearman correlation: SpearmanrResult(correlation=-0.16169603479087277, pvalue=2.093149320427472e-119)
+
+# 50D Glove, humicroedit & funlines
+# original_vs_edit
+# Linear regression equation:  
+# -0.444 x + 1.169
+# Pearson correlation: (-0.1715648018191982, 6.488998669799704e-115)
+# Spearman correlation: SpearmanrResult(correlation=-0.16304199571293118, pvalue=8.038090987863553e-104)
+# n: 17379
+# slope: -0.44402903916586006, intercept: 1.1691089639268526, r_value: -0.17156480181919806, p_value: 6.488998669867637e-115, std_err: 0.019342304195490097
+# RMSE: 0.5870853388736246
+# RMSE average: 0.5931697355562818
+
+# -----------------------------------
+
+# 300D Glove, humicroedit & funlines
+
+# original_vs_edit
+# Linear regression equation:  
+# -0.7467 x + 1.149
+# Pearson correlation: (-0.18115837184710443, 4.072014702863987e-128)
+# Spearman correlation: SpearmanrResult(correlation=-0.16347894968689716, pvalue=2.2433095660085374e-104)
+# n: 17379
+# slope: -0.7466566918314562, intercept: 1.1489431955957161, r_value: -0.18115837184710432, p_value: 4.07201470290964e-128, std_err: 0.030748871050325997
+# RMSE: 0.5862352315243626
+# RMSE average: 0.5931697355562818
+
+# edit_vs_neighbors_window1
+# Linear regression equation:  
+# -0.4588 x + 1.128
+# Pearson correlation: (-0.11093829083263165, 1.4843859979422796e-48)
+# Spearman correlation: SpearmanrResult(correlation=-0.1052615945145242, pvalue=7.3036907375955415e-44)
+# n: 17318
+# slope: -0.4587882821819596, intercept: 1.1280719576001372, r_value: -0.11093829083263154, p_value: 1.4843859979821853e-48, std_err: 0.031233307641369508
+# RMSE: 0.589962526014791
+# RMSE average: 0.5937898849608764
+
+# edit_vs_neighbors_window3
+# Linear regression equation:  
+# -0.6395 x + 1.134
+# Pearson correlation: (-0.13144075331495067, 6.9808207125084545e-68)
+# Spearman correlation: SpearmanrResult(correlation=-0.1315526842123962, pvalue=5.375392992140799e-68)
+# n: 17394
+# slope: -0.6394837242422527, intercept: 1.134273121353309, r_value: -0.1314407533149507, p_value: 6.980820712272878e-68, std_err: 0.03657129385855845
+# RMSE: 0.5888148605096918
+# RMSE average: 0.5933295193064491
+
+# edit_vs_neighbors
+# Linear regression equation:  
+# -0.8018 x + 1.137
+# Pearson correlation: (-0.14850156053117664, 2.4663018291391694e-86)
+# Spearman correlation: SpearmanrResult(correlation=-0.14980314690348023, pvalue=7.736849463456234e-88)
+# n: 17394
+# slope: -0.8018024814984126, intercept: 1.1372279724022023, r_value: -0.14850156053117664, p_value: 2.4663018290518952e-86, std_err: 0.040487348709439736
+# RMSE: 0.5878118266449132
+# RMSE average: 0.5933295193064491
--- code/incongruity.py
+++ code/incongruity.py
@@ -10,6 +10,7 @@ def cosine_similarity(vector1, vector2):
     return np.dot(vector1, vector2) / (len1 * len2)
 
 def original_vs_edit(original_phrase, edit, glove_words, glove_vectors):
+    # calculate similarity (metric 1- original word vs edit word)
     total_similarity = 0
     edit_vector = glove.get_vec(edit.lower(), glove_words, glove_vectors)
     original_words = original_phrase.lower().split()
@@ -19,3 +20,27 @@ def original_vs_edit(original_phrase, edit, glove_words, glove_vectors):
             return None
         total_similarity += cosine_similarity(original_vector, edit_vector)
     return total_similarity/len(original_words)  # Return the average similarity
+
+def edit_vs_neighbors(headline, edit, glove_words, glove_vectors, window_size=None):
+    # find neighbors
+    if window_size is None:
+        window_size = len(headline)
+    start = headline.index('<')
+    end = headline.index('>')
+    words_before = headline[:start].lower().split()[-window_size:]
+    words_after = headline[end+1:].lower().split()[:window_size]
+
+    # calculate similarity (metric 2- edit vs neighbor)
+    total_similarity = 0
+    num_words = 0
+    edit_vector = glove.get_vec(edit.lower(), glove_words, glove_vectors)
+    if edit_vector is None:
+        return None
+    for word in words_before + words_after:
+        vector = glove.get_vec(word, glove_words, glove_vectors)
+        if vector is not None:
+            total_similarity += cosine_similarity(vector, edit_vector)
+            num_words += 1
+    if num_words == 0:
+        return None
+    return total_similarity/num_words
